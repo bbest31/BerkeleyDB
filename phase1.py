1eaac4f28d867e5772298be2d9a897d54450a30a
Brandon Best <bbest@cs.ualberta.ca>
2017-11-21 17:21:04 -0700

    Phase 1 tested and xml file added

--- phase1.py
+++ phase1.py
@@ -2,132 +2,145 @@
 import re
 #iterates through the lines in the data file
 def iterateFile(path):
+    #open file
+    try:
+        termFile = open("terms.txt","w+")
+    except IOError:
+        termFile = open("terms.txt","w+")
+    #grab year and write to years.txt
+    try:
+        yearFile = open("years.txt","w+")
+    except IOError:
+        yearFile = open("years.txt","w+")    
+    #write to recs.text    
+    try:
+        recFile = open("recs.txt","w+")
+    except IOError:
+        recFile = open("recs.txt","w+")
+        
     with open(path) as f:
         #iterates each line
         for line in f:
-            lineType = re.findall(r'(?<=\<).+?(?=\ key)',line)
-            handleLine(lineType[0],line)
+            lineType = re.findall(r'(?<=\<).+?(?=\ )',line)
+            if(lineType != []):
+                handleLine(lineType[0],line,termFile,yearFile,recFile)
             
     
             
 #handles each line from the data file based on its starting tag.
-def handleLine(lineType,line):
+def handleLine(lineType,line,termFile,yearFile,recFile):
     #Do nothing
     if(lineType == "article"):
-        handleArticle(line)
+        handleArticle(line,termFile,yearFile,recFile)
     elif(lineType == "inproceedings"):
-        handleInproceeding
+        handleInproceeding(line,termFile,yearFile,recFile)
 
-def handleArticle(line):
+def handleArticle(line,termFile,yearFile,recFile):
     #grab key
     k = re.findall(r'key="(.*?)"',line)
     key = k[0]
     
     #grab terms and write to terms.txt
-    #open file
-    try:
-        termFile = open("terms.txt")
-    except IOError:
-        termFile = open("terms.txt",'w+')
+    
         
     #write terms from title
-    t = re.findall(r'<title>(.*?)</title>',line)
-    terms = t[0].split(' ')
+    temp = re.findall(r'<title>(.*?)</title>',line)
+    title = []
+    t = temp[0]
+    t = re.sub('[^0-9a-zA-Z]+', ' ',t)
+    title.append(t)
+    terms = title[0].split(' ')
     for term in terms:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
+        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
             termFile.write('t-'+term.lower()+':'+key+'\n')
-            
+       
     #write terms from author
-    auth = re.findall(r'<author>(.*?)</author>',line)
-    authors = auth[0].split(' ')
-    for term in authors:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-            termFile.write('a-'+term.lower()+':'+key)
+    authTemp = re.findall(r'<author>(.*?)</author>',line)
+    auth = []
+    for a in authTemp:
+        a = re.sub('[^0-9a-zA-Z]+', ' ',a)
+        auth.append(a)
+        auth[auth.index(a)] = a.split(' ')
+    for authors in auth:
+        for term in authors:
+            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+                termFile.write('a-'+term.lower()+':'+key+'\n')
     
     #write terms from journal
     j = re.findall(r'<journal>(.*?)</journal>',line)
     journal = j[0].split(' ')
     for term in journal:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-            termFile.write('o-'+term.lower()+':'+key)
+        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+            termFile.write('o-'+term.lower()+':'+key+'\n')
             
     #write terms from publisher
     try:
         p = re.findall(r'<publisher>(.*?)</publisher>',line)
         publisher = p[0].split(' ')
         for term in publisher:
-            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-                termFile.write('o-'+term.lower()+':'+key)            
+            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+                termFile.write('o-'+term.lower()+':'+key+'\n')            
     except:
-        continue
-    #grab year and write to years.txt
-    try:
-        yearFile = open("years.txt")
-    except IOError:
-        yearFile = open("years.txt",'w+')
+        pass
+    
         
     a = re.findall(r'<year>(.*?)</year>',line)
     year = a[0] 
 
     yearFile.write(year+':'+key+'\n')
         
-    #write to recs.text    
-    try:
-        recFile = open("recs.txt")
-    except IOError:
-        recFile = open("recs.txt",'w+')
-        
-    recFile.write(key+':'+line+'\n')
+    
+    recFile.write(key+':'+line)
     
     return 0
 
-def handleInproceeding(line):
+def handleInproceeding(line,termFile,yearFile,recFile):
     #grab key
     k = re.findall(r'key="(.*?)"',line)
     key = k[0]
     
     #grab terms and write to terms.txt
-    #open term file
-    try:
-        termFile = open("terms.txt")
-    except IOError:
-        termFile = open("terms.txt",'w+')
     
     #write the terms in title
-    t = re.findall(r'<title>(.*?)</title>',line)
-    terms = t[0].split(' ')
+    temp = re.findall(r'<title>(.*?)</title>',line)
+    title = []
+    t = temp[0]
+    t = re.sub('[^0-9a-zA-Z]+', ' ',t)
+    title.append(t)
+    terms = title[0].split(' ')
     for term in terms:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
+        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
             termFile.write('t-'+term.lower()+':'+key+'\n')
     
     #write the terms in author
-    auth = re.findall(r'<author>(.*?)</author>',line)
-    authors = auth[0].split(' ')
-    for term in authors:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-            termFile.write('a-'+term.lower()+':'+key)
+    authTemp = re.findall(r'<author>(.*?)</author>',line)
+    auth = []
+    for a in authTemp:
+        a = re.sub('[^0-9a-zA-Z]+', ' ',a)
+        auth.append(a)        
+        auth[auth.index(a)] = a.split(' ')
+    for authors in auth:
+        for term in authors:
+            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+                termFile.write('a-'+term.lower()+':'+key+'\n')
             
     #write the terms in booktitle
     j = re.findall(r'<booktitle>(.*?)</booktitle>',line)
     booktitle = j[0].split(' ')
     for term in booktitle:
-        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-            termFile.write('o-'+term.lower()+':'+key)
+        if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+            termFile.write('o-'+term.lower()+':'+key+'\n')
     #write the terms in publisher
     try:
         p = re.findall(r'<publisher>(.*?)</publisher>',line)
         publisher = p[0].split(' ')
         for term in publisher:
-            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$")):
-                termFile.write('o-'+term.lower()+':'+key)            
+            if(len(term)>2) and (re.match("^[0-9a-zA-Z_]*$",term)):
+                termFile.write('o-'+term.lower()+':'+key+'\n')            
     except:
-        continue
+        pass
     
     #grab year and write to years.txt
-    try:
-        yearFile = open("years.txt")
-    except IOError:
-        yearFile = open("years.txt",'w+')
         
     a = re.findall(r'<year>(.*?)</year>',line)
     year = a[0] 
@@ -135,12 +148,8 @@ def handleInproceeding(line):
     yearFile.write(year+':'+key+'\n')
         
     #write to recs.text    
-    try:
-        recFile = open("recs.txt")
-    except IOError:
-        recFile = open("recs.txt",'w+')
         
-    recFile.write(key+':'+line+'\n')
+    recFile.write(key+':'+line)
     
     return 0
 
@@ -150,12 +159,10 @@ def main():
     if(path == ''):
             print('\nNo data file path given!')
             return False
-    try:
-        iterateFile(path)
-    except:
-        print("Error opening data file.")
-        return False
     
+    iterateFile(path)
+    
+    print("\nData extraction complete!\n")
     return True    
     
     
