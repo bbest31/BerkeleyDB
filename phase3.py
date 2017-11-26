4ca01570d4c86212ed847103ce07482b495ea1d9
Brandon Best <brandonbest94@live.ca>
2017-11-26 15:32:42 -0700

    Phase 3 work

--- phase3.py
+++ phase3.py
@@ -2,8 +2,12 @@
 from bsddb3 import db
 
 #return a list of matches
-def termQry(term,mode,tCursor,yCursor,rCursor):
-   return 0
+def termQry(term,mode,tCursor,rCursor,tDB,rDB):
+   
+
+   matches = tCursor.get(b't-'+ term.encode())
+   #
+   return matches
 
 #queries of the form field:param. Should return a list of matches
 def equivalenceQuery(field,param,mode,tCursor,yCursor,rCursor):
@@ -13,9 +17,13 @@ def equivalenceQuery(field,param,mode,tCursor,yCursor,rCursor):
 def singleRangeQry(field,param,mode,tCursor,yCursor,rCursor):
    return 0
 
+#
+def doubleRangeQry(field,param,mode,yCursor):
+   return 0
+
 #Handles singular clause queries.
-def singleClauseQryHdlr(query,mode,tCursor,yCursor,rCursor):
-   
+def singleClauseQryHdlr(query,mode,tCursor,yCursor,rCursor,tDB,yDB,rDB):
+   matches = []
    #gets the index of the operator
    op = operandIndex(query)
    
@@ -28,11 +36,14 @@ def singleClauseQryHdlr(query,mode,tCursor,yCursor,rCursor):
          param = query[op:]
          
          matches = equivalenceQuery(field,param,mode,tCursor,yCursor,rCursor)
+      elif(query[op] == "<") or (query[op] == ">"):
+         matches = singleRangeQry(field,param,mode,yCursor)
+         
       else:
-         matches = singleRangeQry(field,param,mode,tCursor,yCursor,rCursor):
+         matches = singleRangeQry(field,param,mode,tCursor,yCursor,rCursor)
             
    else:
-      matches = termQry(query,mode,tCursor,yCursor,rCursor)
+      matches = termQry(query,mode,tCursor,rCursor,tDB,rDB)
       
    return matches
 
@@ -40,7 +51,7 @@ def singleClauseQryHdlr(query,mode,tCursor,yCursor,rCursor):
 #We can treat each clause individually and take the intersection of them all.
 #Need to find a way to split the query into clauses without splitting the conditions in the form "term term term"
 #Thinking of replacing any space in the phrase with $(or any unallowed title char), do split() on the while query and then turn the $ back into spaces.
-def multiClauseQryHdlr(query,mode,tCursor,yCursor,rCursor):
+def multiClauseQryHdlr(query,mode,tCursor,yCursor,rCursor,tDB,yDB,rDB):
    
    #Find the range queries and assert that there is at most 2 and they must be different directions <,>
    if(query.count('<')>1) or (query.count('>')>1):
@@ -74,7 +85,7 @@ def multiClauseQryHdlr(query,mode,tCursor,yCursor,rCursor):
       if(qry.count('$')>0):
          qry.replace('$',' ')
          
-      match = singleClauseQryHdlr(qry,mode,tCursor,yCursor,rCursor)
+      match = singleClauseQryHdlr(qry,mode,tCursor,yCursor,rCursor,tDB,yDB,rDB)
       matches.append(match)
       
    #now matches is a list of list which contains matching records in each
@@ -99,15 +110,15 @@ def operandIndex(clause):
       
 #Method to handle the input that are not output changes or program exits but queries.
 #We can then perform queries on the appropriate fields using the specified operator and using the given parameter.
-def queryHandler(query,mode,termsCurs,yearsCurs,recsCurs):
+def queryHandler(query,mode,termsCurs,yearsCurs,recsCurs,tDB,yDB,rDB):
    
    #Need to determine what form the query is in.
    #singular clause:  i.e field:param, field<param, field>param, field:"param phrase"
    #multi clause: i.e. field:param field:param, etc.
    if(query.count(" ") == 0):
-      results = singleClauseQryHdlr(query,mode,termsCurs,yearsCurs,recsCurs)
+      results = singleClauseQryHdlr(query,mode,termsCurs,yearsCurs,recsCurs,tDB,yDB,rDB)
    else:
-      results = multiClauseQryHdlr(query,mode,termsCurs,yearsCurs,recsCurs)
+      results = multiClauseQryHdlr(query,mode,termsCurs,yearsCurs,recsCurs,tDB,yDB,rDB)
    return results
 
 
@@ -164,6 +175,7 @@ def main():
    #This var indicates the output mode.
    # 1 => full; 0 => key
    outputMode = 0
+   
    while(quit == False):
       #Indicating the current output format.
       if(outputMode == 1):
@@ -176,6 +188,12 @@ def main():
       #may need a for loop to go through and any alpha char call .lower()
       #Decision making with input.
       if(query == 'Q') or (query == 'q'):
+         termsDB.close
+         yearsDB.close
+         recsDB.close
+         termsCurs.close
+         yearsCurs.close
+         recsCurs.close
          quit = True
          break
       elif(query.replace(" ",'') == "output=full"):
@@ -187,19 +205,22 @@ def main():
          print("\n========|Output Format Changed|========")
       #Handle Query
       else:
-         results = queryHandler(query,outputMode,termsCurs,yearsCurs,recsCurs)
+         results = queryHandler(query,outputMode,termsCurs,yearsCurs,recsCurs,termsDB,yearsDB,recsDB)
+         
          #Need to format print based on mode
          if(len(results) == 0):
             print("\nNo matches found.")
          #Print in key mode
          elif(outputMode == 0):
             for r in results:
+               r = str(r)
                print("Key: "+r+'\n')
          #Print in full mode
          else:
             for r in results:
+               r =str(r)
                #Will format this later
-               print('\n'+r)
+               print(r)
                
          print("\n========|^Results^|========")
    print("\n========|Program Closed|========")
